79035d67c101e5613b8c70d98a922be37ceda92d
antoninifabio <31709728+antoninifabio@users.noreply.github.com>
2025-09-17 23:45:49 +0100

    Create download.sh
    download data set

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OUTDIR="zenodo_downloads"
+mkdir -p "$OUTDIR"
+
+FILES=(
+  "injectionDict_O1O2O3O4a_FAR_1_in_1_semianalytic_SNR_10.pickle"
+  "sampleDict_FAR_1_in_1_yr.pickle"
+  "sampleDict_O4a_cat_4c4fd2cef_717_date_250401.npy"
+)
+ 
+for f in "${FILES[@]}"; do
+  wget -P "$OUTDIR" "https://zenodo.org/records/17148537/files/$f"
+done
+
+echo "All files downloaded to $OUTDIR/"
